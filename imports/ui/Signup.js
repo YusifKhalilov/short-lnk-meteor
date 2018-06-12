44022c3e9da9727c7006525ee41da5704a5d498b
Yusif Khalilov <joseph.khalilov@gmail.com>
2018-06-12 02:39:57 -0700

    Update sign up language

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -16,14 +16,14 @@ export default class Signup extends React.Component {
     let password = this.refs.password.value.trim();
 
     if (password.length < 9) {
-      return this.setState({error: 'Password must be more than 8 characters long'});
+      return this.setState({ error: 'Password must be more than 8 characters long' });
     }
 
-    Accounts.createUser({email, password}, (err) => {
+    Accounts.createUser({ email, password }, (err) => {
       if (err) {
-        this.setState({error: err.reason});
+        this.setState({ error: err.reason });
       } else {
-        this.setState({error: ''});
+        this.setState({ error: '' });
       }
     });
   }
@@ -36,12 +36,12 @@ export default class Signup extends React.Component {
           {this.state.error ? <p>{this.state.error}</p> : undefined}
 
           <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form">
-            <input type="email" ref="email" name="email" placeholder="Email"/>
-            <input type="password" ref="password" name="password" placeholder="Password"/>
+            <input type="email" ref="email" name="email" placeholder="Email" />
+            <input type="password" ref="password" name="password" placeholder="Password" />
             <button className="button">Create Account</button>
           </form>
 
-          <Link to="/">Already have an account?</Link>
+          <Link to="/">Have an account?</Link>
         </div>
       </div>
     );
